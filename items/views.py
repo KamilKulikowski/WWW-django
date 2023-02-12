baa8c678c4adc5d1a870c8b672db74897a1c84cd
KamilKulikowski <113850596+KamilKulikowski@users.noreply.github.com>
2023-02-12 21:13:17 +0100

    Dodanie plików

---
+++ items/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django import forms
+from .models import Item
+class ItemCreateForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = '__all__'
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def item_create(request):
+    if request.method == "POST":
+        form = ItemCreateForm(request.POST)
+        if form.is_valid():
+            form.save()
+        else:
+            print(form.errors)
+    else:
+        form = ItemCreateForm()
+    return render(request, 'items/item_create.html', {'form': form})
